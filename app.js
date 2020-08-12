b30529f5336dad04c3bf9582c49205cc17f51d4c
daisy <zacaopingzi@gmail.com>
2020-08-12 23:21:21 +1000

    modify some features

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 // mongoose.set('useFindAndModify', false);
 
-const homeContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeContent = "You are a ukulele beyond my microphone. You are a Yukon beyond my Micronesia. You are a union beyond my meiosis. You are a eureka beyond my maitai. You are a Yuletide beyond my minesweeper.You are a euphemism beyond my myna bird."
+const aboutContent = "Hi! I am Daisy. Welcome to my Blog. I love to post my thoughts."
+const contactContent = "Hi there, Here is Daisy. 😊 You can find me via below methods. Looking forward to eeting you."
 
 const app = express();
 dotenv.config();
@@ -23,7 +20,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // setup public folder for static files
 app.use(express.static("public"));
 
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 // Post Schema
 const postSchema = {
@@ -72,29 +73,14 @@ app.get("/posts/:postId", function (req, res) {
   const requestedPostId = req.params.postId;
   Post.findById({ _id: requestedPostId }, function (err, post) {
     if (!err) {
-    res.render("post", {
-      title: post.title,
-      content: post.content
-    });
-  } else {
-    res.send(err);
-  }
-  });
-});
-
-// Delete the post using ID
-app.post("/posts/:postId", function(req, res){
-  console.log(req.params.postId);
-  Post.findByIdAndDelete(
-    {_id: req.params.postId},
-    function(err){
-      if (!err){
-        res.redirect("/");
-      } else {
-        res.send(err);
-      }
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    } else {
+      res.send(err);
     }
-  );
+  });
 });
 
 app.listen(3000, function () {
